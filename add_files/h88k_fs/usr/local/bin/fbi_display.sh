49f8de8055cb92c5682488897b5689adf2cf1b59
unifreq <flippy@sina.com>
2024-10-14 21:19:46 +0800

    update fbi_display.sh

--- add_files/h88k_fs/usr/local/bin/fbi_display.sh
+++ add_files/h88k_fs/usr/local/bin/fbi_display.sh
@@ -18,48 +18,73 @@ if ! dpkg -l fbi >/dev/null 2>&1;then
 	exit 1
 fi
 
-size=240x135
+SIZE=240x135
 tty=6
-while :;do
-	img=$(mktemp --suffix='.png')
-	convert -size $size \
-        	xc:red \
-        	-fill white \
-        	-pointsize 36 \
-        	-gravity NorthWest \
-       		-draw 'text 20,24 "Hlink H88K"' \
-        	-draw 'text 20,64 "Hello world!"' \
-        	$img
-	fbi -d $fbdev -t 1 -1 -T $tty $img >/dev/null 2>&1
-	sleep 5
-	rm -f $img
+delay=3
 
-	img=$(mktemp --suffix='.png')
-	day=$(date +%Y-%m-%d)
-	time=$(date +"  %H:%M:%S  ")
-	convert -size $size \
-        	xc:blue \
-        	-fill white \
-        	-pointsize 36 \
-        	-gravity NorthWest \
-       		-draw "text 20,24 \"$day\"" \
-        	-draw "text 20,64 \"$time\"" \
-        	$img
-	fbi -d $fbdev -t 1 -1 -T $tty $img >/dev/null 2>&1
-	sleep 5
-	rm -f $img
-
-	for img in /usr/local/share/h88k/imgs/*;do
-		this_size=$(identify -format "%wx%h" $img 2>/dev/null)
-		if [ "$this_size" != "" ];then
-			case $this_size in 
-				$size) zoom=""
-				       ;;
-				    *) zoom="-autozoom"
-				       ;;
-			esac
-			fbi -d $fbdev -t 1 -1 -T $tty $zoom $img >/dev/null 2>&1
-			sleep 5
+function display_img() {
+	local fbdev=$1
+	local tty=$2
+	local img=$3
+	local delay=$4
+	local rm_img=$5
+	local pid=""
+	local size=$(identify -format "%wx%h" $img 2>/dev/null)
+	if [ "$size" != "" ];then
+		case $size in 
+			$SIZE) zoom=""
+				;;
+			    *) zoom="-autozoom"
+				;;
+		esac
+		fbi -d $fbdev -T $tty $zoom $img >/dev/null 2>&1 &
+		sleep $delay
+		pid=$(ps -ef | grep "fbi \-d" | grep -v grep | awk '{print $2}')
+		[ "$pid" != "" ] && kill -9 $pid 2>/dev/null
+		if [ "$rm_img" == "1" ];then
+			rm $img
 		fi
-	done
+	fi
+}
+
+# Blank screen
+img=$(mktemp --suffix='.png')
+convert -size $SIZE \
+       	xc:black \
+       	-fill white \
+       	-pointsize 36 \
+       	-gravity NorthWest \
+	-draw 'text 20,24 ""' \
+       	$img
+display_img $fbdev $tty $img 1 1
+
+# Hello world
+img=$(mktemp --suffix='.png')
+convert -size $SIZE \
+       	xc:red \
+       	-fill white \
+       	-pointsize 36 \
+       	-gravity NorthWest \
+	-draw 'text 20,24 "Hlink H88K"' \
+       	-draw 'text 20,64 "Hello world!"' \
+       	$img
+display_img $fbdev $tty $img $delay 1
+
+# Date & time
+img=$(mktemp --suffix='.png')
+day=$(date +%Y-%m-%d)
+time=$(date +"  %H:%M:%S  ")
+convert -size $SIZE \
+       	xc:blue \
+       	-fill white \
+       	-pointsize 36 \
+       	-gravity NorthWest \
+	-draw "text 20,24 \"$day\"" \
+       	-draw "text 20,64 \"$time\"" \
+       	$img
+display_img $fbdev $tty $img $delay 1
+
+# Images
+for img in /usr/local/share/h88k/imgs/*;do
+	display_img $fbdev $tty $img $delay 0
 done
